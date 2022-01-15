50f519e6e09f8d4ed10119ad9cfa7207225b8469
Danial Noaein <sdnoaein@gmail.com>
2022-01-15 23:54:27 +0330

    add Form Elements KEYUP Event

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
-///////////////////////
-// Get Form Elements //
-///////////////////////
+//==========================
+//=== Get Form Elements ====
+//==========================
 const form = document.querySelector("form");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 
-////////////////////////
-// Handle Submit Form //
-////////////////////////
+//===========================
+//=== Handle Submit Form ====
+//===========================
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+});
+
+//=========================================
+//=== Handle Form Elements KEYUP Event ====
+//=========================================
+emailInput.addEventListener("keyup", () => {
+  console.log(emailInput.value);
+});
+
+passwordInput.addEventListener("keyup", () => {
+  console.log(passwordInput.value);
 });

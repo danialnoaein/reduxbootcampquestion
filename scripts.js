2d1379eaf87d228b2050e5a0444e9ddaf21ac964
Danial Noaein <sdnoaein@gmail.com>
2022-01-16 00:01:47 +0330

    add password validator

--- scripts.js
+++ scripts.js
@@ -5,24 +5,6 @@ const form = document.querySelector("form");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 
-//===========================
-//=== Handle Submit Form ====
-//===========================
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-});
-
-//=========================================
-//=== Handle Form Elements KEYUP Event ====
-//=========================================
-emailInput.addEventListener("keyup", () => {
-  emailInput.style.borderColor = checkGmail(emailInput.value) ? "green" : "red";
-});
-
-passwordInput.addEventListener("keyup", () => {
-  console.log(passwordInput.value);
-});
-
 //===================================
 //=== Check functions with regex ====
 //===================================
@@ -34,3 +16,87 @@ const checkGmail = (emailAddress) => {
     return false;
   }
 };
+
+const checkLength = (password) => {
+  let pattern = /^.{6,}$/;
+  if (password.match(pattern)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkHasDigit = (password) => {
+  let pattern = /^.*[0-9]/;
+  if (password.match(pattern)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkHasUpperCaseLetter = (password) => {
+  let pattern = /^.*[A-Z]/;
+  if (password.match(pattern)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkHasLowerCaseLetter = (password) => {
+  let pattern = /^.*[a-z]/;
+  if (password.match(pattern)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkHasSpecialLetter = (password) => {
+  let pattern = /^.*[!@#$%]/;
+  if (password.match(pattern)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//===================
+//=== Validators ====
+//===================
+const validatePassword = (emailAddress) => {
+  return checkGmail(emailAddress);
+};
+
+const validatePassword = (password) => {
+  return (
+    checkLength(password) &&
+    checkHasDigit(password) &&
+    checkHasUpperCaseLetter(password) &&
+    checkHasLowerCaseLetter(password) &&
+    checkHasSpecialLetter(password)
+  );
+};
+
+//=========================================
+//=== Handle Form Elements KEYUP Event ====
+//=========================================
+emailInput.addEventListener("keyup", () => {
+  emailInput.style.borderColor = validatePassword(emailInput.value)
+    ? "green"
+    : "red";
+});
+
+passwordInput.addEventListener("keyup", () => {
+  passwordInput.style.borderColor = validatePassword(passwordInput.value)
+    ? "green"
+    : "red";
+});
+
+//===========================
+//=== Handle Submit Form ====
+//===========================
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
